126ed8638678aa6b7ff96e7d888b3477fca90ee7
Overlander <anuragnandal@hotmail.com>
2022-05-17 07:53:52 -0400

    Add methods for setup and player creation

---
+++ lib/game.rb
@@ -0,0 +1,28 @@
+require_relative 'player'
+
+class Game
+  attr_reader :player1
+
+  SYMBOLS = ['⚫', '⚪']
+
+  def setup
+    intro_message
+    @player1 = create_player(1)
+    @player2 = create_player(2)
+  end
+
+  def intro_message
+    puts "Let's play Connect Four! We'll begin by having each player choose a name."
+  end
+
+  def create_player(number)
+    puts "Player #{number}, enter your name:"
+    name = gets.chomp
+    symbol = SYMBOLS[number - 1]
+    Player.new(name, symbol)
+  end
+
+  def play_game
+    setup
+  end
+end
