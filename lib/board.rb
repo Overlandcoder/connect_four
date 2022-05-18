b183441fbea00719dd87a5b887832c87f809dfa5
Overlander <anuragnandal@hotmail.com>
2022-05-18 11:03:07 -0400

    Modify #display

--- lib/board.rb
+++ lib/board.rb
@@ -2,19 +2,24 @@ class Board
   attr_reader :board
 
   def initialize
-    create_board
+    @board = Array.new(6, Array.new(8))
   end
 
-  def create
-    @board = Array.new(6, Array.new(7))
+  # ignore
+  def insert_numbers(i = 0)
+    return if i == 6
+
+    board[i][0] = (i + 1) * 10
+    insert_numbers(i + 1)
   end
 
   def display(num = 0)
     return if num == 6
 
-    board[num].each_index { |i| board[num][i] = "a" }
-    p board[num].join(' | ')
-    puts '---------------------------'
-    display_board(num + 1)
+    board.each do |array|
+      #array.each_index { |i| board[num][i] = '⚫' if i > 0 }
+    end
+    p board[num]
+    display(num + 1)
   end
 end
