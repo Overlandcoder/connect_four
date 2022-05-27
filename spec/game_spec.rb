fe3496bf739b9f444b102cb3e0c414bccf2462e4
Overlander <anuragnandal@hotmail.com>
2022-05-27 16:54:30 -0400

    Finish tests for #play_game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,10 @@
 require_relative '../lib/game'
+require 'pry-byebug'
 
 describe Game do
   subject(:game) { described_class.new }
   let(:player1) { double(Player, name: 'John', symbol: '⚫') }
   let(:board) { double(Board) }
-
-  describe '#play_game' do
-  end
 
   describe '#create_player' do
     context 'when creating player 1' do
@@ -78,39 +76,27 @@ describe Game do
   end
 
   describe '#play_game' do
-    context 'when #board.full? is false four times' do
+    context 'when #board.game_over? is false four times' do
       before do
         allow(game).to receive(:setup)
         allow(game).to receive(:current_player).and_return(player1)
-        allow(game).to receive(:gets).and_return('1\n')
-        allow(board).to receive(:full?).and_return(false, false, false, false, true)
+        allow(game.board).to receive(:game_over?).with(player1.symbol).and_return(false, false, false, false, true)
       end
 
       it 'calls #switch_turns four times' do
-        allow(board).to receive(:display)
+        allow(game).to receive(:play_round)
         expect(game).to receive(:switch_turns).exactly(4).times
         game.play_game
       end
 
-      it 'calls #prompt_player four times' do
-        allow(board).to receive(:display)
-        expect(game).to receive(:prompt_player).exactly(4).times
+      it 'calls #play_round four times' do
+        expect(game).to receive(:play_round).exactly(4).times
         game.play_game
       end
 
-      xit 'calls #solicit_move four times' do
-        expect(game).to receive(:solicit_move).exactly(4).times
-        game.play_game
-      end
-
-      xit 'sends #place_token to board' do
-        allow(game).to receive(:solicit_move).and_return(1)
-        expect(board).to receive(:place_token).with(1, '⚫').exactly(4).times
-        game.play_game
-      end
-
-      xit 'sends #display to board' do
-        expect(board).to receive(:display).exactly(4).times
+      it 'calls #conclusion once' do
+        allow(game).to receive(:play_round)
+        expect(game).to receive(:conclusion).once
         game.play_game
       end
     end
